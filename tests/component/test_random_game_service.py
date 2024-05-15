d667c3dd45c385a3842854bd918345a31d97aede
ILINSK <103816860+ILINSK@users.noreply.github.com>
2024-05-15 23:33:58 +0300

    Update test_random_game_service.py

--- tests/component/test_random_game_service.py
+++ tests/component/test_random_game_service.py
@@ -3,10 +3,7 @@ import requests
 base_url = 'http://localhost:8001'
 
 
-def test_get_game_by_id():
-    response = requests.get(f"{base_url}/game/1") 
-    assert response.status_code == 200
-    assert 'name' in response.json()
+
 
 
 def test_get_games():
